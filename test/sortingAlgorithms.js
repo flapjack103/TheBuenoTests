ba3aed7a25aa1cfcc3e1ba982e55e1806dbb3adb
Alexandra Grant <alexandrag103@gmail.com>
2015-02-01 11:32:32 -0500

    strict mode

--- test/sortingAlgorithms.js
+++ test/sortingAlgorithms.js
@@ -1,21 +1,38 @@
+'use strict';
+// Sorting algorithm tests
 var assert = require('assert'),
-sort = require('../sortingAlgorithms');
+sort = require('../lib/sortingAlgorithms');
 
 describe('Sorting Algorithms', function() {
 
-    for(func in sort) {
-        describe('#'+func, function() {
+    var funcs = ['bubblesort', 'mergesort', 'insertionsort', 'quicksort'];
+
+    for(var i = 0; i < funcs.length; i++) {
+        describe('#', function() {
+            var num = i;
             it('should sort for array with complete reverse order', function() {
-              assert.deepEqual([1,2,3,4,5], sort[func]([5,4,3,2,1]));
+                (function() {
+                    var type = funcs[num];
+                    assert.deepEqual([1,2,3,4,5], sort[type]([5,4,3,2,1]));
+              }());
             });
             it('should sort for an array with length 1', function() {
-              assert.deepEqual([1], sort[func]([1]));
+                (function() {
+                  var type = funcs[num];
+                  assert.deepEqual([1], sort[type]([1]));
+                }());
             });
             it('should sort for an array with multiple equal values', function() {
-              assert.deepEqual([3,3,3,5,5,7,7,7], sort[func]([7,3,7,5,3,3,5,7]));
+              (function() {
+                var type = funcs[num];
+                assert.deepEqual([3,3,3,5,5,7,7,7], sort[type]([7,3,7,5,3,3,5,7]));
+              }());
             });
             it('should return an empty array if given an empty array', function() {
-                assert.deepEqual([], sort[func]([]));
+              (function() {
+                var type = funcs[num];
+                assert.deepEqual([], sort[type]([]));
+              }());
             });
         });
     }
