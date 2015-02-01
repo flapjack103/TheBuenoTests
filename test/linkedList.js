2e2ea6eec0a361175316acd8932aaf1c839410ae
Alexandra Grant <alexandrag103@gmail.com>
2015-02-01 11:34:16 -0500

    stub file for linked list tests

---
+++ test/linkedList.js
@@ -0,0 +1,14 @@
+'use strict';
+// Linked list operations tests
+var LinkedList = require('../lib/linkedList')
+var list = new LinkedList();
+
+// TODO
+// list.add('cat');
+// list.add('dog');
+// list.add('fish');
+// list.insertAfter('dog', 'bird');
+// list.insertAfter('fish', 'iguana');
+// list.print();
+// list.reverse();
+// list.print();
